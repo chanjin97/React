a0b13336c5782360531409f6626d16009894ae58
chanjin97 <chanjindark@naver.com>
2024-04-04 12:52:50 +0900

    input_modal

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
-import React, { useState } from "react";
-import "./assets/css/style.scss";
+import React from "react";
 
 function App() {
-  let [viewData, setViewData] = useState([]);
-  let [textData, setTextData] = useState("");
-
-  function clickListener() {
-    let copy = [...viewData];
-    copy.push(textData);
-    setViewData(copy);
-    setTextData(""); // input value 제거
-  }
-  function textDataChange(event) {
-    console.log(event.target.value);
-    setTextData(event.target.value);
-  }
-  return (
-    <div>
-      {viewData}
-      <div>
-        <label htmlFor="name">이름</label>
-        <input
-          type="text"
-          id="name"
-          onChange={textDataChange}
-          value={textData}
-        />
-        {/* <input type="text" id="name" onChange={함수}/>
-        <input type="text" id="name" onChange={()=>{함수()}}/>
-        <input type="text" id="name" onChange={()=>{setTextData("변경할것")}}/> */}
-      </div>
-      <div>
-        <button onClick={clickListener}>입력</button>
-      </div>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
---
+++ src/App_input.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./assets/css/style.scss";
+import ModalView from "./components/ModalView";
+
+function App() {
+  let [viewData, setViewData] = useState([]);
+  let [textData, setTextData] = useState("");
+  let [mView, setmView] = useState(false);
+
+  function modalView() {
+    setmView(true);
+  }
+  function clickListener() {
+    let copy = [...viewData];
+    copy.push(textData);
+    setViewData(copy);
+    setTextData(""); // input value 제거
+  }
+  function textDataChange(event) {
+    console.log(event.target.value);
+    setTextData(event.target.value);
+  }
+  return (
+    <div>
+      {/* {viewData} */}
+      <div className="inpwrap">
+        <label htmlFor="name">이름</label>
+        <input
+          type="text"
+          id="name"
+          onChange={textDataChange}
+          value={textData}
+          placeholder="입력하세요"
+        />
+        {/* <input type="text" id="name" onChange={함수}/>
+        <input type="text" id="name" onChange={()=>{함수()}}/>
+        <input type="text" id="name" onChange={()=>{setTextData("변경할것")}}/> */}
+
+        <button onClick={clickListener}>입력</button>
+      </div>
+
+      <ul className="menupan">
+        {/* {viewData.map(function(){})} */}
+        {viewData.map((item, idx) => {
+          return (
+            <div>
+              <li
+                onClick={() => {
+                  modalView(idx);
+                }}
+              >
+                {viewData[idx]}
+              </li>
+              {mView == true ? <ModalView /> : null}
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function Modal(props) {}
+export default App;
