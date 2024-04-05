b3de47abc4f66d41bbbf209c8b26bd8fba6c8cc7
chanjin97 <chanjindark@naver.com>
2024-04-05 14:52:20 +0900

    pagenation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-import React, { useState } from "react";
-import "./assets/css/style02.scss";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./assets/css/style.scss";
 
 function App() {
-  const [num, setNum] = useState(1);
-  return <></>;
+  const [viewData, setViewData] = useState([]);
+  const [totalPage, setTotalPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [listCnt, setListCnt] = useState(10);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://jsonplaceholder.typicode.com/posts?_page=${currentPage}&_limit=${listCnt}`
+      )
+      .then((res) => {
+        console.log(res);
+        setViewData(res.data);
+
+        console.log(res.headers["x - total - count"]);
+        let totalRecord = res.headers["x - total - count"];
+        let totalPage = Math.ceil(totalRecord / listCnt);
+        setTotalPage(totalPage);
+      })
+      .catch((err) => {
+        console.eroor(err);
+      });
+  }, [currentPage]);
+
+  const pageNumber = () => {
+    // const page = 5
+    // let startPage = ;
+    // let endPage = ;
+    // let number = "";
+    // for (let i = startPage; i <= endPage; i++) {
+    //   number += "";
+    // }
+    // return number;
+  };
+
+  return (
+    <>
+      <div>
+        {totalPage}
+        {viewData.map((item) => {
+          return (
+            <>
+              <div>{item.title}</div>
+            </>
+          );
+        })}
+      </div>
+
+      <div className="pagination">
+        <div>이전</div>
+        <ul className="page">{pageNumber}</ul>
+        <div>다음</div>
+      </div>
+    </>
+  );
 }
 
 export default App;
