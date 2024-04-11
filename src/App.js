b96f75ec5cdedcd4cf1aac60cfff9449c7063c63
chanjin97 <chanjindark@naver.com>
2024-04-11 20:02:38 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,54 +1,117 @@
 import React from "react";
 import "./assets/css/tStyle.scss";
+//npm i redux react-redux
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
+
+// persist 설정 lib
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const initialState = {
+  num: 0,
+  title: "안녕하세요",
+  content:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod asperiores, quas maxime aliquid excepturi accusamus quos dolorem temporibus dolor beatae.",
+};
+
+function reducer(state = initialState, action) {
+  // console.log("init data : " + state.num);
+  console.log(action);
+  if (action.type === "numUp") {
+    return {
+      ...state /* state 깊은복사 */,
+      num: state.num + action.payload,
+    };
+  }
+
+  if (action.type === "titleModi") {
+    return {
+      ...state,
+      title: action.text,
+    };
+  }
+
+  return state;
+}
+
+// persist구성;
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(persistedReducer);
+// const store = createStore(reducer, initialState);
+
+const persitor = persistStore(store);
 
 function App() {
   return (
-    <>
-      {/* <div className="container mx-auto rounded-md text-white hover:bg-sb-950 transition  bg-gray-500 grid gird-cols-1 md:grid-cols-3 gap-4 mt-5">
-        <div className="bg-sb-300 rounded-md p-4 ">test1</div>
-        <div className="bg-sb-300 rounded-md p-4 ">test2</div>
-        <div className="bg-sb-300 rounded-md p-4 ">test3</div>
-        <div className="bg-sb-300 rounded-md p-4 md:hidden">test4</div>
-      </div> */}
-      <section className="flex  mt-20 max-w-[400px] m-auto">
-        <div className="bg-white shadow-md rounded-md border p-6 w-full">
-          <h1 className="text-center text-2xl font-semibold mb-4">로그인</h1>
-          <hr className="mb-4" />
-          <div className="mb-6">
-            <label
-              htmlFor="email"
-              className="text-sm font-semibold text-gray-500 mb-2 flex"
-            >
-              이메일
-            </label>
-            <input
-              type="text"
-              id="email"
-              className="w-full bg-white border rounded-md p-2 text-xs"
-              placeholder="이메일을 입력하세요"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="pwd"
-              className="text-sm font-semibold text-gray-500 mb-2 flex"
-            >
-              패스워드
-            </label>
-            <input
-              type="text"
-              id="pwd"
-              className="w-full bg-white border rounded-md p-2 text-xs"
-              placeholder="패스워드를 입력하세요"
-            />
-          </div>
-          <div>
-            <button className="w-full text-xs text-white bg-gray-800 px-4 py-2 rounded-md hover:bg-gray-600">
-              회원가입
-            </button>
-          </div>
+    <div>
+      <Provider store={store}>
+        <div className="border p-4">
+          <h3>title1</h3>
+          <ChildOne />
         </div>
-      </section>
+      </Provider>
+    </div>
+  );
+}
+
+function ChildOne() {
+  const title = useSelector((state) => {
+    return state.title;
+  });
+  return (
+    <>
+      <div className="border p-4">
+        <h3>childone {title} </h3>
+        <ChildTwo />
+      </div>
+    </>
+  );
+}
+
+function ChildTwo() {
+  const num = useSelector((state) => {
+    return state.num;
+  });
+  const title = useSelector((state) => {
+    return state.title;
+  });
+
+  // const {num, content} = useSelector((state) => {
+  //   return state;
+  // });
+  // const {num, content, title} = useSelector((state) => state);
+  const dispatch = useDispatch();
+  console.log(" child trans : " + num);
+  return (
+    <>
+      <div className="border p-4">
+        <h3>childTwo</h3>
+        number ( {num} )<br />
+        title : {title}
+        <br />
+        <button
+          className=" bg-blue-300 px-4 py-1 rounded-md hover:bg-blue-500 hover:text-white"
+          onClick={() => {
+            dispatch({ type: "numUp", payload: 1 });
+          }}
+        >
+          아주쉬운클릭
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "titleModi", text: "title변경됨" });
+          }}
+        >
+          글자변경
+        </button>
+      </div>
     </>
   );
 }
